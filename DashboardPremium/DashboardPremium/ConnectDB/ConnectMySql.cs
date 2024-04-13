49d786f4aa6796379b23313f4aa66847a615a38e
“Vustey” <vutv579@gmail.com>
2024-04-14 00:16:43 +0700

    Update_ConnectDB

---
+++ DashboardPremium/DashboardPremium/ConnectDB/ConnectMySql.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace DashboardPremium.ConnectDB
+{
+    public class ConnectMySql
+    {
+
+        string strconn = "Server=localhost;Port=3306;Database=test;Uid=root;Pwd=040220;";
+        MySqlConnection connection;
+        public MySqlConnection Connectsql()
+        {
+            strconn = "Server=127.0.0.1;Port=3306;Database=mydb;Uid=root;Pwd=Vu21012003@;";
+            connection = new MySqlConnection(strconn);
+            try
+            {
+                connection.Open();
+                return connection;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ DashboardPremium/DashboardPremium/ConnectDB/ConnectSqlServer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace DashboardPremium.ConnectDB
+{
+    public class ConnectSqlServer
+    {
+        string sql = @"Data Source=MSI;Initial Catalog=item;Integrated Security=True";
+        SqlConnection conn;
+        public ConnectSqlServer()
+        {
+            sql = @"Data Source=MSI;Initial Catalog=HRM;Integrated Security=True";
+            conn = new SqlConnection(sql);
+        }
+    }
+}
