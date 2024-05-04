283cf4d60aac63a741b92ec9a2c8ae29054c39bf
TranVuThanhLam <thanhlam20062003@gmail.com>
2024-05-04 16:05:08 +0700

    View is now can open in visual studio

--- DashboardPremium/DashboardPremium/Controllers/HomeController.cs
+++ DashboardPremium/DashboardPremium/Controllers/HomeController.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace DashboardPremium.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        // Đầu tiên Route sẽ gọi đến hành động HomePageView
+        public ActionResult HomePageView()
         {
+            // Trả về trang có tên là HomePageView.cshtml
             return View();
         }
 
